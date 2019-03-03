f7549e2a512670c81e01391501fa2d285a936198
Jerry1712 <jerryphilip0007@gmail.com>
2019-03-03 19:49:26 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -9,21 +10,31 @@ import { environment } from '../environments/environment';
 import { PostsComponent } from './posts/posts.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ThemeService } from './services/theme.service';
+import { NewsService } from './services/news.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    SharedModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [ThemeService, NewsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
